ea16a79e67dc0851e4aac125b664b3fd9b864a24
Muhammad Talha Khan <utubefortk@gmail.com>
2026-05-28 15:08:41 +0500

    Update task_model.dart

--- lib/models/task_model.dart
+++ lib/models/task_model.dart
@@ -1,1 +1,12 @@
+class Task {
+  final String id;
+  String title;
+  bool isCompleted;
 
+  Task({required this.id, required this.title, this.isCompleted = false});
+
+  // A handy method to toggle the completion state
+  void toggleCompleted() {
+    isCompleted = !isCompleted;
+  }
+}
